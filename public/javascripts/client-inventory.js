891f6014121356af3af7afe30ff8ba27ef60c155
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-05-03 17:24:00 +0700

    Add event handler for the thumbnail's upload event  - Allow to preview thumbnail before upload
     - Display the original name of the thumbnail image

--- public/javascripts/client-inventory.js
+++ public/javascripts/client-inventory.js
@@ -1,4 +1,13 @@
 $(document).ready(() => {
+    $("#thumbnailInp").change(function () {
+        var thumbnail = $(this).prop('files')[0]
+        $("#imgPreview").attr("src", URL.createObjectURL(thumbnail))
+        $("#imgPreview").on("load", function() {
+            URL.revokeObjectURL($(this).attr("src"));
+        })
+        $("#thumbnailLabel").text(thumbnail.name);
+    })
+
     // Detect change on price input and format the value
     $("#priceInp").on("input", function () {
         var formattedPrice = parseFloat($(this).val().replace(/,/g, '')).toLocaleString('en');
