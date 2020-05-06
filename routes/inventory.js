8963fdb8d81c3d74af4a5ecff2ef547b9f4468ef
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-05-06 12:33:24 +0700

    Update the controller run on POST --> /inventory
     - Update other things as well

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,7 +1,30 @@
-var router = require("express").Router({mergeParams: true})
+var router = require("express").Router({ mergeParams: true }),
+    populate = require("./utils/upload-img"), // Populate multipart/form-data
+    fs = require("fs"),
+    Toy = require("../models/Toy")
 
 router.get("/", (_req, res) => {
-    res.render("management", {route: "inventory"})
+    res.render("management", { route: "inventory" })
+})
+
+router.post("/", populate.any(), (req, res) => {
+    var toy = req.body.toy;
+    toy.thumbnail = "/images/uploaded/" + req.files[0].filename;
+    Toy.create(toy)
+        .then((newToy) => res.status(201).json(newToy))
+        .catch((err) => {
+            fs.unlink("./" + req.files[0].path, () => { }) // Delete the saved image of the error item
+            next(err)
+        })
+})
+
+router.put("/:toyID", populate.any(), (req, res) => {
+    console.log(req.body)
+    console.log(req.params.toyID)
+})
+
+router.delete("/:toyID", (req, res) => {
+
 })
 
 module.exports = router
