d479dc42b5bea015c57eb152ddbc2127de652191
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-05-07 11:30:44 +0700

    Add click event handler on .edit-btn /customers

---
+++ public/javascripts/client-customers.js
@@ -0,0 +1,15 @@
+$("tbody").on("click", ".edit-btn", function() {
+    var rowPieces = $(this).parent().siblings();
+
+    var name = $(rowPieces[1]).attr("data-name"),
+        age = $(rowPieces[2]).attr("data-age"),
+        gender = $(rowPieces[3]).attr("data-gender"),
+        address = $(rowPieces[4]).attr("data-address"),
+        phoneNumb = $(rowPieces[5]).attr("data-phone-numb")
+
+    $("#nameInp").val(name)
+    $("#ageInp").val(age)
+    $(`input[name='customer[gender]'][value=${gender}]`).prop('checked', true);
+    $("#addressInp").val(address)
+    $("#phoneNumbInp").val(phoneNumb)
+})
