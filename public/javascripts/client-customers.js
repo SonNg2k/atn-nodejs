3510d739c5962c81157e888f929116e6600bd68d
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-05-07 17:28:10 +0700

    Remove #ageInp related JS code and add new code

--- public/javascripts/client-customers.js
+++ public/javascripts/client-customers.js
@@ -2,13 +2,13 @@ $("tbody").on("click", ".edit-btn", function() {
     var rowPieces = $(this).parent().siblings();
 
     var name = $(rowPieces[1]).attr("data-name"),
-        age = $(rowPieces[2]).attr("data-age"),
+        dob = $(rowPieces[2]).attr("data-dob"),
         gender = $(rowPieces[3]).attr("data-gender"),
         address = $(rowPieces[4]).attr("data-address"),
         phoneNumb = $(rowPieces[5]).attr("data-phone-numb")
 
     $("#nameInp").val(name)
-    $("#ageInp").val(age)
+    $("#dobInp").val(dob)
     $(`input[name='customer[gender]'][value=${gender}]`).prop('checked', true);
     $("#addressInp").val(address)
     $("#phoneNumbInp").val(phoneNumb)
@@ -16,7 +16,7 @@ $("tbody").on("click", ".edit-btn", function() {
 
 function clearInps() {
     $("#nameInp").val("")
-    $("#ageInp").val("")
+    $("#dobInp").val("")
     $("input[type=radio]").prop('checked', false);
     $("#addressInp").val("")
     $("#phoneNumbInp").val("")
