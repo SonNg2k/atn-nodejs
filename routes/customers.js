46b31019a208fa9789cffdfaebca926d93f43aaa
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-05-07 18:29:43 +0700

    Complete contoller for POST --> /customers

--- routes/customers.js
+++ routes/customers.js
@@ -1,7 +1,16 @@
-var router = require("express").Router({mergeParams: true})
+var router = require("express").Router({ mergeParams: true }),
+    populate = require("./utils/upload-img"), // Populate multipart/form-data
+    Customer = require("../models/Customer")
 
 router.get("/", (_req, res) => {
-    res.render("management", {route: "customers"})
+    res.render("management", { route: "customers" })
+})
+
+router.post("/", populate.any(), (req, res, next) => {
+    var customer = req.body.customer
+    Customer.create(customer)
+        .then((newClient) => res.status(201).json(newClient))
+        .catch(next)
 })
 
 module.exports = router
