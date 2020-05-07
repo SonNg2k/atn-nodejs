6a6a42d256320bd803710fbde03e42a0fc1d6259
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-05-07 18:25:53 +0700

    Define addRow() in client-customer.js

--- public/javascripts/client-customers.js
+++ public/javascripts/client-customers.js
@@ -14,6 +14,27 @@ $("tbody").on("click", ".edit-btn", function() {
     $("#phoneNumbInp").val(phoneNumb)
 })
 
+function addRow(client) {
+    var row = `
+    <tr style="background-color: #FFEFD5;">
+        <th scope="row" data-id="${client._id}">New</th>
+        <td data-name="${client.name}">${client.name}</td>
+        <td data-dob="${HTMLinputDate(client.dob)}">${friendlyDate(client.dob)}</td>
+        <td data-gender="${client.gender}">${client.gender}</td>
+        <td data-address="${client.address}">${client.address}</td>
+        <td data-phone-numb="${client.phoneNumb}">${client.phoneNumb}</td>
+        <td data-invoice="">None</td>
+        <td>
+            <button type="button" class="edit-btn btn btn-success" data-toggle="modal"
+                data-target="#versatileModal">Edit <i class="fas fa-edit"></i></button>
+            <button type="button" class="delete-btn btn btn-danger" data-toggle="modal"
+                data-target="#deleteConfirmModal">Delete <i class="fas fa-trash"></i></button>
+        </td>
+    </tr>
+    `
+    $("tbody").prepend(row)
+}
+
 function clearInps() {
     $("#nameInp").val("")
     $("#dobInp").val("")
@@ -28,4 +49,8 @@ function getFullFormData(formData) {
 
 function friendlyDate (ISO_date) {
     return new Date(ISO_date).toDateString().slice(4)
+}
+
+function HTMLinputDate(ISO_date) {
+    return ISO_date.slice(0, 10)
 }
