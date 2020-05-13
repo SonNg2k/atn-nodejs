ce54e530b6cdc6cb969467bd73e6d344667bf0be
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-05-13 12:35:53 +0700

    Complete schema for Invoice

--- models/Invoice.js
+++ models/Invoice.js
@@ -1,7 +1,27 @@
 var mongoose = require("mongoose")
 
 var InvoiceSchema = new mongoose.Schema({
-
+    date: {
+        type: Date,
+        required: true,
+        min: '1950-01-01',
+        max: '2015-12-31'
+    },
+    total: {
+        type: Number,
+        required: true,
+        min: 0,
+        default: 0
+    },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Customer",
+    },
+    // A collection of purchased items...
+    details: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "InvoiceItem"
+    }]
 })
 
 module.exports = mongoose.model("Invoice", InvoiceSchema)
