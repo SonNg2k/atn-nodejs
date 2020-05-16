4a5374e3a98bef784fd2b095f1f335270cc79871
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-05-16 12:25:34 +0700

    Complete controller for GET --> /invoices

--- routes/invoices.js
+++ routes/invoices.js
@@ -1,7 +1,15 @@
-var router = require("express").Router({mergeParams: true})
+var router = require("express").Router({mergeParams: true}),
+    Customer = require("../models/Customer"),
+    Toy = require("../models/Toy")
 
-router.get("/", (_req, res) => {
-    res.render("management", {route: "invoices"})
+
+router.get("/", (_req, res, next) => {
+    // Get a  a list of customers. One of them might be selected for the invoice
+    Promise.all([Customer.find({}).lean(), Toy.find({}).lean()])
+    .then((found) => {
+        res.render("management", {route: "invoices", clients: found[0], toys: found[1]})
+    })
+    .catch(next)
 })
 
 module.exports = router
