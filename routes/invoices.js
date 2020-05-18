8cdad284813354c7c6b7ab5054dc087ffaa7029f
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-05-18 14:15:26 +0700

    Complete the POST controller at /invoices

--- routes/invoices.js
+++ routes/invoices.js
@@ -1,15 +1,44 @@
-var router = require("express").Router({mergeParams: true}),
+var router = require("express").Router({ mergeParams: true }),
+    populate = require("./utils/upload-img"),
     Customer = require("../models/Customer"),
-    Toy = require("../models/Toy")
+    Toy = require("../models/Toy"),
+    Invoice = require("../models/Invoice"),
+    InvoiceItem = require("../models/InvoiceItem")
 
 
 router.get("/", (_req, res, next) => {
     // Get a  a list of customers. One of them might be selected for the invoice
     Promise.all([Customer.find({}).lean(), Toy.find({}).lean()])
-    .then((found) => {
-        res.render("management", {route: "invoices", clients: found[0], toys: found[1]})
-    })
-    .catch(next)
+        .then((found) => {
+            res.render("management", { route: "invoices", clients: found[0], toys: found[1] })
+        })
+        .catch(next)
+})
+
+router.post("/", populate.any(), async (req, res, next) => {
+    var invoice = req.body.invoice,
+        invoiceItem = req.body.item
+    try {
+        invoiceItem = await InvoiceItem.create(invoiceItem) // First try to create an invoice
+        // If the given invoice is created successfully, it means that the given invoice's item is valid
+        // Add values to the following properties of the invoice
+        invoice.total = invoiceItem.subtotal
+        invoice.invoiceItem = invoiceItem._id
+
+        // Then create the invoice...
+        invoice = await Invoice.create(invoice)
+        // Populate data to the properties of type mongoose.Schema.Types.ObjectId...
+        invoice = await invoice.populate("customer", "-invoices").populate("invoiceItem").execPopulate()
+
+        // Find the toy included in the invoice...
+        var includedToy = await Toy.findById(req.body.item._toyID, "name").lean()
+
+        // Then add the name and price of the toy...
+        invoice.invoiceItem._doc.name = includedToy.name; // ._doc is the actual data returned to client
+        res.status(201).json(invoice)
+    } catch (e) {
+        next(e)
+    }
 })
 
 module.exports = router
