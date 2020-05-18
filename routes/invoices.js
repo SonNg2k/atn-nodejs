44d5f2756bb3793250ea7ca21d1481188716c9cb
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-05-18 19:54:14 +0700

    Update GET and POST controllers at route /invoices

--- routes/invoices.js
+++ routes/invoices.js
@@ -3,38 +3,45 @@ var router = require("express").Router({ mergeParams: true }),
     Customer = require("../models/Customer"),
     Toy = require("../models/Toy"),
     Invoice = require("../models/Invoice"),
-    InvoiceItem = require("../models/InvoiceItem")
+    InvoiceLine = require("../models/InvoiceLine")
 
 
 router.get("/", (_req, res, next) => {
     // Get a  a list of customers. One of them might be selected for the invoice
-    Promise.all([Customer.find({}).lean(), Toy.find({}).lean()])
+    Promise.all([Customer.find({}).lean(), Toy.find({}).lean(),
+    Invoice.find({}).populate("customer", "-invoices").populate({
+        path: "invoiceLine",
+        populate: {
+            path: "toy",
+            model: "Toy"
+        }
+    }).lean()])
         .then((found) => {
-            res.render("management", { route: "invoices", clients: found[0], toys: found[1] })
+            res.render("management", { route: "invoices", clients: found[0], toys: found[1], invoices: found[2]})
         })
         .catch(next)
 })
 
 router.post("/", populate.any(), async (req, res, next) => {
     var invoice = req.body.invoice,
-        invoiceItem = req.body.item
+        invoiceLine = req.body.invoiceLine
     try {
-        invoiceItem = await InvoiceItem.create(invoiceItem) // First try to create an invoice
+        invoiceLine = await InvoiceLine.create(invoiceLine) // First try to create an invoice
         // If the given invoice is created successfully, it means that the given invoice's item is valid
         // Add values to the following properties of the invoice
-        invoice.total = invoiceItem.subtotal
-        invoice.invoiceItem = invoiceItem._id
+        invoice.total = invoiceLine.subtotal
+        invoice.invoiceLine = invoiceLine._id
 
         // Then create the invoice...
         invoice = await Invoice.create(invoice)
         // Populate data to the properties of type mongoose.Schema.Types.ObjectId...
-        invoice = await invoice.populate("customer", "-invoices").populate("invoiceItem").execPopulate()
+        invoice = await invoice.populate("customer", "-invoices").populate("invoiceLine").execPopulate()
 
         // Find the toy included in the invoice...
-        var includedToy = await Toy.findById(req.body.item._toyID, "name").lean()
+        var includedToy = await Toy.findById(req.body.invoiceLine.toy, "name").lean()
 
         // Then add the name and price of the toy...
-        invoice.invoiceItem._doc.name = includedToy.name; // ._doc is the actual data returned to client
+        invoice.invoiceLine._doc.name = includedToy.name; // ._doc is the actual data returned to client
         res.status(201).json(invoice)
     } catch (e) {
         next(e)
