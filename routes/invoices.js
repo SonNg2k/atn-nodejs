70c3969a15c74d6b29552aa3a4dd8a84c3b43cce
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2020-05-19 15:33:00 +0700

    Complete the PUT, DELETE controllers for /invoices

--- routes/invoices.js
+++ routes/invoices.js
@@ -10,14 +10,14 @@ router.get("/", (_req, res, next) => {
     // Get a  a list of customers. One of them might be selected for the invoice
     Promise.all([Customer.find({}).lean(), Toy.find({}).lean(),
     Invoice.find({}).populate("customer", "-invoices").populate({
-        path: "invoiceLine",
-        populate: {
+        path: "invoiceLine", // Each invoice has a property 'invoiceLine' pointing to the InvoiceLine model
+        populate: { // Each invoiceLine document has a property 'toy' pointing to the Toy model
             path: "toy",
             model: "Toy"
         }
     }).lean()])
         .then((found) => {
-            res.render("management", { route: "invoices", clients: found[0], toys: found[1], invoices: found[2]})
+            res.render("management", { route: "invoices", clients: found[0], toys: found[1], invoices: found[2] })
         })
         .catch(next)
 })
@@ -43,6 +43,41 @@ router.post("/", populate.any(), async (req, res, next) => {
         // Then add the name and price of the toy...
         invoice.invoiceLine._doc.name = includedToy.name; // ._doc is the actual data returned to client
         res.status(201).json(invoice)
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.put("/:invoiceID", populate.any(), async (req, res, next) => {
+    var newDate = req.body.invoice.date
+    try {
+        // 1) Create a new InvoiceLine document, which will then be assigned to the existing Invoice document
+        var newLineItem = await InvoiceLine.create(req.body.invoiceLine)
+        newLineItem = await newLineItem.populate("toy").execPopulate()
+
+        // 2) Update the invoice with the given ID
+        var newInvoice = {
+            date: newDate,
+            total: newLineItem.subtotal,
+            invoiceLine: newLineItem._id
+        }
+        var foundInvoice = await Invoice.findByIdAndUpdate(req.params.invoiceID, newInvoice).lean()
+        await InvoiceLine.findByIdAndDelete(foundInvoice.invoiceLine)
+
+        // 3) Return data to client
+        newInvoice.invoiceLine = newLineItem
+        res.status(201).json(newInvoice)
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.delete("/:invoiceID", async (req, res, next) => {
+    try {
+        // First find the invoice
+        var foundInvoice = await Invoice.findByIdAndDelete(req.params.invoiceID).lean()
+        await InvoiceLine.findByIdAndDelete(foundInvoice.invoiceLine)
+        res.status(201).json("success")
     } catch (e) {
         next(e)
     }
